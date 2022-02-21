6280715f6607854e2591005d5fc3db41d3ebc675
Yuliyan <uliqnvasilev@gmail.com>
2022-02-21 08:58:38 +0100

    Use Bulma FrameWork for Style

--- app.js
+++ app.js
@@ -21,8 +21,12 @@ p1Button.addEventListener('click', function () {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+
+            p1Button.disabled = true;
+            p2Button.disabled = true;
+
         }
         p1Display.textContent = p1Score;
     }
@@ -36,8 +40,11 @@ p2Button.addEventListener('click', function () {
         p2Score += 1;
         if (p2Score === winningScore) {
             isGameOver = true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
@@ -63,7 +70,10 @@ function reset() {
     p1Display.textContent = 0;
     p2Display.textContent = 0;
 
-    p1Display.classList.remove('winner', 'loser'); // Remove Color After Reset for Player One
-    p2Display.classList.remove('winner', 'loser'); // Remove Color After Reset for Player Two
+    p1Display.classList.remove('has-text-success', 'has-text-danger'); // Remove Color After Reset for Player One
+    p2Display.classList.remove('has-text-success', 'has-text-danger'); // Remove Color After Reset for Player Two
 
+
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
